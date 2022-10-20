e3fa169f2da393768ace9f57cd09661002bb5c6e
ImJihoon <112602028+ImJihoon@users.noreply.github.com>
2022-10-20 16:17:04 -0500

    Update StrafeCodeTest.java
    added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/StrafeCodeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/StrafeCodeTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp (name = "strafe")
 
 public class StrafeCodeTest extends  OpMode {
+    //wheels
     DcMotor leftFront;
     DcMotor rightFront;
     DcMotor leftBack;
@@ -13,6 +14,7 @@ public class StrafeCodeTest extends  OpMode {
 
     @Override
     public void init() {
+        //wheel motor hardware map
         leftFront = hardwareMap.dcMotor.get("motor1");
         rightFront = hardwareMap.dcMotor.get("motor2");
         leftBack = hardwareMap.dcMotor.get("motor3");
@@ -22,22 +24,24 @@ public class StrafeCodeTest extends  OpMode {
 
     @Override
     public void loop() {
+        //defining Wheel power
         double leftFrontPower;
         double rightFrontPower;
         double leftBackPower;
         double rightBackPower;
-
+        
+        //giving controls a value to use for drive
         double straight = gamepad1.right_stick_y;
         double strafing = gamepad1.right_stick_x;
         double turn = gamepad1.left_stick_x;
 
+        //strafe equation
         leftFrontPower = (straight + strafing + turn);
         rightFrontPower = (straight - strafing - turn);
         leftBackPower = (straight + strafing + turn);
         rightBackPower = (straight - strafing - turn);
 
-
-
+        //strafe chassis wheel move
         leftFront.setPower(leftFrontPower);
         rightFront.setPower(rightFrontPower);
         leftBack.setPower(leftBackPower);
