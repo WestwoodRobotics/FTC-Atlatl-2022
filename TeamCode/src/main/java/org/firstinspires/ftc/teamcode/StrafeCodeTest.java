150cddc073fe0315b5ff87f4453b7883cb1fc772
Jihoon Im <0919imjihoon@gmail.com>
2022-10-20 17:41:51 -0500

    Fixed error in StrafeCodeTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeCodeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StrafeCodeTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.drive.opmode;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
