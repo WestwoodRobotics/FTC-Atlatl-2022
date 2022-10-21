4870eae2e02706fab80be3b817d06a508943a0e3
ImJihoon <112602028+ImJihoon@users.noreply.github.com>
2022-10-21 11:09:31 -0500

    Create TeleOpAtl.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAtl.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "strafe")
+
+public class TeleOpAtl extends OpMode {
+    //wheels
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftBack = null;
+    DcMotor rightBack = null;
+
+    //lift and intake
+    DcMotor lift = null;
+    Servo intake = null;
+
+    @Override
+    public void init() {
+        //wheel motor hardware map
+        leftFront = hardwareMap.get(DcMotor.class,"leftFront");
+        rightFront = hardwareMap.get(DcMotor.class,"rightFront");
+        leftBack = hardwareMap.get(DcMotor.class,"leftBack");
+        rightBack = hardwareMap.get(DcMotor.class,"rightBack");
+        //Lift and intake hardware map
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        intake = hardwareMap.get(Servo.class, "intake");
+
+    }
+
+    @Override
+    public void loop() {
+        //defining Wheel power
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+
+        //giving controls a value to use for drive
+        double straight = gamepad1.right_stick_y;
+        double strafing = gamepad1.right_stick_x;
+        double turn = gamepad1.left_stick_x;
+
+        //strafe equation
+        leftFrontPower = (straight + strafing + turn);
+        rightFrontPower = (straight - strafing - turn);
+        leftBackPower = (straight + strafing + turn);
+        rightBackPower = (straight - strafing - turn);
+
+        //strafe chassis wheel move
+        leftFront.setPower(leftFrontPower);
+        rightFront.setPower(rightFrontPower);
+        leftBack.setPower(leftBackPower);
+        rightBack.setPower(rightBackPower);
+        lift.setPower(0.1);
+    }
+}
