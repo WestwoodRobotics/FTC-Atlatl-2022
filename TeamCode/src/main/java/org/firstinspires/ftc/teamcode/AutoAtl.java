84a8083eda7d1c611bd841dbf7b9cd1f2a952fac
Jihoon Im <0919imjihoon@gmail.com>
2022-10-24 16:19:59 -0500

    Added base setup for auto, changed some names for TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoAtl.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Autonomous(name = "AutoAtl")
+public class AutoAtl extends OpMode {
+    //wheels
+    DcMotor leftFront = null;
+    DcMotor rightFront = null;
+    DcMotor leftBack = null;
+    DcMotor rightBack = null;
+
+    //lift and intake
+    DcMotor lift = null;
+    Servo intake = null;
+
+    @Override
+    public void init() {
+        //wheel motor hardware map
+        leftFront = hardwareMap.get(DcMotor.class,"leftFront");
+        rightFront = hardwareMap.get(DcMotor.class,"rightFront");
+        leftBack = hardwareMap.get(DcMotor.class,"leftBack");
+        rightBack = hardwareMap.get(DcMotor.class,"rightBack");
+        //Lift and intake hardware map
+        lift = hardwareMap.get(DcMotor.class, "lift");
+        intake = hardwareMap.get(Servo.class, "intake");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAtl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAtl.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "CringeTeleOp")
+@TeleOp(name = "TeleOpAtl")
 
 public class TeleOpAtl extends OpMode {
     //wheels
