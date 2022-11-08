501127e957bde3ee7946147849a4baca23a188bf
Jihoon Im <0919imjihoon@gmail.com>
2022-11-08 12:59:15 -0600

    lift system v3.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAtl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpAtl.java
@@ -37,6 +37,9 @@ public class TeleOpAtl extends OpMode {
         lift = hardwareMap.get(DcMotor.class, "lift");
         intake = hardwareMap.get(Servo.class, "intake");
 
+        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
         lift.setDirection(DcMotor.Direction.FORWARD);
         intake.setDirection(Servo.Direction.REVERSE);
     }
@@ -75,7 +78,22 @@ public class TeleOpAtl extends OpMode {
 
 
         //lift & intake
-        lift.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+        if (gamepad2.a){
+            lift.setTargetPosition(20);
+            lift.setPower(0.3);
+        }
+        if (gamepad2.b){
+            lift.setTargetPosition(100);
+            lift.setPower(0.3);
+        }
+        if (gamepad2.x){
+            lift.setTargetPosition(200);
+            lift.setPower(0.3);
+        }
+        if (gamepad2.y){
+            lift.setTargetPosition(300);
+            lift.setPower(0.3);
+        }
 
     }
 }
