039a1726eac7fb99581a76636a632988ef5d52ed
jihoo <0919imjihoon@gmail.com>
2022-11-11 21:36:05 -0600

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleDriverTeleOpAtl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleDriverTeleOpAtl.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "TeleOpAtlDouble")
+@TeleOp(name = "DoNotUse")
 
 public class DoubleDriverTeleOpAtl extends OpMode {
     //wheels
