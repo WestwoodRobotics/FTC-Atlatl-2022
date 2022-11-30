a2727e01cca2c3d35f5fc0e2f6c1fe68d6afebcb
jihoo <0919imjihoon@gmail.com>
2022-11-29 22:48:49 -0600

    ser position tester code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleDriverTeleOpAtl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DoubleDriverTeleOpAtl.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -21,13 +20,8 @@ public class DoubleDriverTeleOpAtl extends OpMode {
     public Servo intake = null;
 
     public int intakePressed = 0;
-    public boolean autoLift = false;
-    public double liftPower;
-    public int liftTarget = 0;
-    public int dpadPressed = 0;
-    public boolean slowMode = false;
     public int slowModePressed = 0;
-
+    public boolean slowMode;
 
     @Override
     public void init() {
@@ -49,7 +43,6 @@ public class DoubleDriverTeleOpAtl extends OpMode {
 
         lift.setDirection(DcMotor.Direction.FORWARD);
         intake.setDirection(Servo.Direction.REVERSE);
-        intake.setPosition(0);
 
         lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -60,163 +53,90 @@ public class DoubleDriverTeleOpAtl extends OpMode {
 
     @Override
     public void loop() {
-        //drive chassis
-        {
-            //defining Wheel power
-            double leftFrontPower;
-            double rightFrontPower;
-            double leftBackPower;
-            double rightBackPower;
+        //defining Wheel power
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+
+        //giving controls a value to use for drive
+
+        double straight = gamepad1.right_stick_y;
+        double strafing = gamepad1.right_stick_x;
+        double turn = (gamepad1.left_stick_x * 0.7);
 
 
-            //giving controls a value to use for drive
-            double straight = gamepad1.right_stick_y;
-            double strafing = gamepad1.right_stick_x;
-            double turn = (gamepad1.left_stick_x * 0.7);
+        //strafe equation
+        leftFrontPower = (straight - strafing - turn);
+        rightFrontPower = (straight + strafing + turn);
+        leftBackPower = (straight + strafing - turn);
+        rightBackPower = (straight - strafing + turn);
 
-
-            //strafe equation
-            leftFrontPower = (straight - strafing - turn);
-            rightFrontPower = (straight + strafing + turn);
-            leftBackPower = (straight + strafing - turn);
-            rightBackPower = (straight - strafing + turn);
-
-
-            //strafe chassis wheel move
-            if ((liftPos > 2000) || slowMode) {
-                leftFront.setPower(leftFrontPower * 0.3);
-                rightFront.setPower(rightFrontPower * 0.3);
-                leftBack.setPower(leftBackPower * 0.3);
-                rightBack.setPower(rightBackPower * 0.3);
-            } else {
-                leftFront.setPower(leftFrontPower);
-                rightFront.setPower(rightFrontPower);
-                leftBack.setPower(leftBackPower);
-                rightBack.setPower(rightBackPower);
-            }
-
+        //strafe chassis wheel move
+        if (slowMode){
+            leftFront.setPower(leftFrontPower * 0.4);
+            rightFront.setPower(rightFrontPower * 0.4);
+            leftBack.setPower(leftBackPower * 0.4);
+            rightBack.setPower(rightBackPower * 0.4);
+        }else{
+            leftFront.setPower(leftFrontPower);
+            rightFront.setPower(rightFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightBack.setPower(rightBackPower);
         }
 
 
-        //liftPos variable
+
+
+        //lift
+
         liftPos = lift.getCurrentPosition();
+        telemetry.addData("Lift Power: ", -gamepad2.left_trigger + gamepad2.right_trigger);
+        if ((-gamepad1.left_trigger + gamepad1.right_trigger) > 0) {
 
-
-        //autoLift switcher
-        {
-            if ((gamepad2.dpad_down) && dpadPressed == 0)
-                autoLift = !autoLift;
-            dpadPressed++;
-            if (!(gamepad2.dpad_down) && dpadPressed > 0)
-                dpadPressed = 0;
-        }
-
-
-        //lift power and position set
-        {
-            if (autoLift) {
-                lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                lift.setTargetPosition((int) liftPos);
-
-                //down
-                if (gamepad2.a) {
-                    liftTarget = 0;
-                    telemetry.addData("Target position: ", "ground");
-                }
-
-                //low
-                if (gamepad2.b) {
-                    liftTarget = 500;
-                    telemetry.addData("Target position: ", "low");
-                }
-
-                //mid
-                if (gamepad2.x) {
-                    liftTarget = 1700;
-                    telemetry.addData("Target position: ", "mid");
-                }
-
-                //high
-                if (gamepad2.y) {
-                    liftTarget = 3400;
-                    telemetry.addData("Target position: ", "high");
-                }
-
-                lift.setTargetPosition(liftTarget);
-
-                if (liftPos > liftTarget) {
-                    //going down
-                    lift.setPower(-0.4);
-                } else if (liftPos < liftTarget) {
-                    //going up
-                    lift.setPower(0.7);
-                } else {
-                    //stopping
-                    lift.setPower(0);
-                }
-
+            if (liftPos < 3400) {
+                telemetry.addData("lift dir: ", "up");
+                lift.setPower((-gamepad2.left_trigger + gamepad2.right_trigger));
             } else {
-                lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 lift.setPower(0);
-                //manual lift
-                if ((-gamepad2.left_trigger + gamepad2.right_trigger) > 0) {
-
-                    if (liftPos < 3400) {
-                        telemetry.addData("lift dir: ", "up");
-                        lift.setPower((-gamepad2.left_trigger + gamepad2.right_trigger));
-                    } else {
-                        lift.setPower(0);
-                    }
-
-                } else if ((-gamepad2.left_trigger + gamepad2.right_trigger) < 150) {
-                    if (liftPos > 0) {
-                        telemetry.addData("lift dir:", "down");
-                        lift.setPower((-gamepad2.left_trigger + gamepad2.right_trigger));
-                    } else {
-                        lift.setPower(0);
-                    }
-
-                } else {
-                    lift.setPower(0);
-                }
             }
+
+        } else if ((-gamepad2.left_trigger + gamepad2.right_trigger) < 150) {
+            if (liftPos > 0) {
+                telemetry.addData("lift dir:", "down");
+                lift.setPower((-gamepad2.left_trigger + gamepad2.right_trigger));
+            } else {
+                lift.setPower(0);
+            }
+        } else {
+            lift.setPower(0);
         }
 
         //intake
-        {
-            if ((gamepad2.left_bumper || gamepad2.right_bumper) && intakePressed == 0) {
-                if (intake.getPosition() == 0.76) {
-                    intake.setPosition(1);
-                    telemetry.addData("intake", "open");
-                } else {
-                    intake.setPosition(0.76);
-                    telemetry.addData("intake", "closed");
-                }
-                intakePressed++;
+        if ((gamepad2.left_bumper || gamepad2.right_bumper) && intakePressed == 0) {
+            if (intake.getPosition()==0.7) {
+                intake.setPosition(1);
+            } else {
+                intake.setPosition(0.7);
             }
-            if ((!gamepad2.left_bumper && !gamepad2.right_bumper) && intakePressed > 0) {
-                intakePressed = 0;
-            }
+            intakePressed++;
         }
-
+        if ((!gamepad2.left_bumper || gamepad2.right_bumper) && intakePressed > 0) {
+            intakePressed = 0;
+        }
         //slow mode toggle
-        {
-            if ((gamepad1.left_bumper || gamepad1.right_bumper) && slowModePressed == 0)
-                slowMode = !slowMode;
+        if ((gamepad1.right_bumper) && slowModePressed == 0) {
+            slowMode = !slowMode;
             slowModePressed++;
-
-            if ((!gamepad1.left_bumper && !gamepad1.right_bumper) && slowModePressed > 0)
-                slowModePressed = 0;
-
         }
+        if ((!gamepad1.right_bumper) && slowModePressed > 0) {
+            slowModePressed = 0;
+        }
+        telemetry.addData("slow Mode", slowMode);
         //telemetry
-        {
-            telemetry.addData("slow mode", slowMode);
-            telemetry.addData("lift position", liftPos);
-            telemetry.addData("lift power", liftPower);
+        telemetry.addData("lift position: ", liftPos);
+        telemetry.addData("servo state: ", intake.getPosition());
+        telemetry.update();
 
-
-            telemetry.update();
-        }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SoloSetPosTest.java
@@ -0,0 +1,187 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "set pos test")
+
+public class SoloSetPosTest extends OpMode {
+    //wheels
+    public DcMotorEx leftFront = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx leftBack = null;
+    public DcMotorEx rightBack = null;
+    public double liftPos;
+
+    //lift and intake
+    public DcMotorEx lift = null;
+    public Servo intake = null;
+
+    public int intakePressed = 0;
+    public int slowModePressed = 0;
+    public boolean slowMode;
+    public boolean autoLift;
+    public int autoLiftPressed = 0;
+
+    public int liftTarget;
+    @Override
+    public void init() {
+        //wheel motor hardware map
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
+
+
+        //Lift and intake hardware map
+        lift = hardwareMap.get(DcMotorEx.class, "lift");
+        intake = hardwareMap.get(Servo.class, "intake");
+
+        lift.setDirection(DcMotorEx.Direction.FORWARD);
+        intake.setDirection(Servo.Direction.REVERSE);
+
+        lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        lift.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+    }
+
+
+    @Override
+    public void loop() {
+        //defining Wheel power
+        double leftFrontPower;
+        double rightFrontPower;
+        double leftBackPower;
+        double rightBackPower;
+
+        //giving controls a value to use for drive
+
+        double straight = gamepad1.right_stick_y;
+        double strafing = gamepad1.right_stick_x;
+        double turn = (gamepad1.left_stick_x * 0.7);
+
+
+        //strafe equation
+        leftFrontPower = (straight - strafing - turn);
+        rightFrontPower = (straight + strafing + turn);
+        leftBackPower = (straight + strafing - turn);
+        rightBackPower = (straight - strafing + turn);
+
+        //strafe chassis wheel move
+        if (slowMode){
+            leftFront.setPower(leftFrontPower * 0.4);
+            rightFront.setPower(rightFrontPower * 0.4);
+            leftBack.setPower(leftBackPower * 0.4);
+            rightBack.setPower(rightBackPower * 0.4);
+        }else{
+            leftFront.setPower(leftFrontPower);
+            rightFront.setPower(rightFrontPower);
+            leftBack.setPower(leftBackPower);
+            rightBack.setPower(rightBackPower);
+        }
+
+
+
+
+        //lift
+
+        liftPos = lift.getCurrentPosition();
+        if (autoLift) {
+            lift.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+            telemetry.addData("Lift Power: ", -gamepad1.left_trigger + gamepad1.right_trigger);
+            if ((-gamepad1.left_trigger + gamepad1.right_trigger) > 0) {
+
+                if (liftPos < 3400) {
+                    telemetry.addData("lift dir: ", "up");
+                    lift.setPower((-gamepad1.left_trigger + gamepad1.right_trigger));
+                } else {
+                    lift.setPower(0);
+                }
+
+            } else if ((-gamepad1.left_trigger + gamepad1.right_trigger) < 150) {
+                if (liftPos > 0) {
+                    telemetry.addData("lift dir:", "down");
+                    lift.setPower((-gamepad1.left_trigger + gamepad1.right_trigger));
+                } else {
+                    lift.setPower(0);
+                }
+            } else {
+                lift.setPower(0);
+            }
+        }else{
+            lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            //target position setting
+            {
+                if (gamepad1.a) {
+                    liftTarget=0;
+                } else if (gamepad1.b) {
+                    liftTarget=1000;
+                } else if (gamepad1.x) {
+                    liftTarget=2000;
+                } else if (gamepad1.y) {
+                    liftTarget=3640;
+                }
+            }
+
+            //power setting
+            lift.setTargetPosition(liftTarget);
+            lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            if (liftTarget > liftPos) {
+                lift.setPower(.8);
+            } else if (liftPos > liftTarget) {
+                lift.setPower(-.8);
+            } else if (liftPos == liftTarget) {
+                lift.setPower(0);
+            }
+
+        }
+
+        //intake
+        if ((gamepad1.left_bumper) && intakePressed == 0) {
+            if (intake.getPosition()==0.7) {
+                intake.setPosition(1);
+            } else {
+                intake.setPosition(0.7);
+            }
+            intakePressed++;
+        }
+        if ((!gamepad1.left_bumper) && intakePressed > 0) {
+            intakePressed = 0;
+        }
+        //slow mode
+        if ((gamepad1.right_bumper) && slowModePressed == 0) {
+            slowMode = !slowMode;
+            slowModePressed++;
+        }
+        if ((!gamepad1.right_bumper) && slowModePressed > 0) {
+            slowModePressed = 0;
+        }
+
+
+
+
+        if ((gamepad1.dpad_down) && autoLiftPressed == 0) {
+            autoLift = !autoLift;
+            autoLiftPressed++;
+        }
+        if ((!gamepad1.dpad_down) && autoLiftPressed > 0) {
+            autoLiftPressed = 0;
+        }
+
+
+        telemetry.addData("slow Mode", slowMode);
+        //telemetry
+        telemetry.addData("lift position: ", liftPos);
+        telemetry.addData("servo state: ", intake.getPosition());
+        telemetry.update();
+
+    }
+}
