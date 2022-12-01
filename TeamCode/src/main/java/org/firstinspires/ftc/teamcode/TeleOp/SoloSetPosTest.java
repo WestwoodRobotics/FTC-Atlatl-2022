9f9b8cd45259927eafeb78828d3e52666d686dfb
jihoo <0919imjihoon@gmail.com>
2022-11-30 22:47:06 -0600

    new folder to put teleop in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SoloSetPosTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/SoloSetPosTest.java
@@ -1,52 +1,54 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "SoloAtl")
+@TeleOp(name = "set pos test")
 
-public class SoloDriverTeleOpAtl extends OpMode {
+public class SoloSetPosTest extends OpMode {
     //wheels
-    public DcMotor leftFront = null;
-    public DcMotor rightFront = null;
-    public DcMotor leftBack = null;
-    public DcMotor rightBack = null;
+    public DcMotorEx leftFront = null;
+    public DcMotorEx rightFront = null;
+    public DcMotorEx leftBack = null;
+    public DcMotorEx rightBack = null;
     public double liftPos;
 
     //lift and intake
-    public DcMotor lift = null;
+    public DcMotorEx lift = null;
     public Servo intake = null;
 
     public int intakePressed = 0;
     public int slowModePressed = 0;
     public boolean slowMode;
 
+    public int liftTarget;
     @Override
     public void init() {
         //wheel motor hardware map
-        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
 
-        leftFront.setDirection(DcMotor.Direction.REVERSE);
-        rightFront.setDirection(DcMotor.Direction.REVERSE);
-        leftBack.setDirection(DcMotor.Direction.REVERSE);
-        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+        rightBack.setDirection(DcMotorEx.Direction.REVERSE);
 
 
         //Lift and intake hardware map
-        lift = hardwareMap.get(DcMotor.class, "lift");
+        lift = hardwareMap.get(DcMotorEx.class, "lift");
         intake = hardwareMap.get(Servo.class, "intake");
 
-        lift.setDirection(DcMotor.Direction.FORWARD);
+        lift.setDirection(DcMotorEx.Direction.FORWARD);
         intake.setDirection(Servo.Direction.REVERSE);
 
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lift.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
 
     }
 
@@ -73,12 +75,12 @@ public class SoloDriverTeleOpAtl extends OpMode {
         rightBackPower = (straight - strafing + turn);
 
         //strafe chassis wheel move
-        if (slowMode){
+        if (slowMode) {
             leftFront.setPower(leftFrontPower * 0.4);
             rightFront.setPower(rightFrontPower * 0.4);
             leftBack.setPower(leftBackPower * 0.4);
             rightBack.setPower(rightBackPower * 0.4);
-        }else{
+        } else {
             leftFront.setPower(leftFrontPower);
             rightFront.setPower(rightFrontPower);
             leftBack.setPower(leftBackPower);
@@ -86,31 +88,50 @@ public class SoloDriverTeleOpAtl extends OpMode {
         }
 
 
-
-
         //lift
-
         liftPos = lift.getCurrentPosition();
-        telemetry.addData("Lift Power: ", -gamepad1.left_trigger + gamepad1.right_trigger);
-        if ((-gamepad1.left_trigger + gamepad1.right_trigger) > 0) {
 
-            if (liftPos < 3400) {
-                telemetry.addData("lift dir: ", "up");
-                lift.setPower((-gamepad1.left_trigger + gamepad1.right_trigger));
-            } else {
-                lift.setPower(0);
+        //manual
+        if (!(gamepad1.right_trigger-gamepad1.left_trigger == 0)) {
+            /*
+            if (gamepad1.right_trigger > 0) {
+                liftTarget += 20;
+            } else if (gamepad1.left_trigger > 0) {
+                liftTarget -= 20;
             }
-
-        } else if ((-gamepad1.left_trigger + gamepad1.right_trigger) < 150) {
-            if (liftPos > 0) {
-                telemetry.addData("lift dir:", "down");
-                lift.setPower((-gamepad1.left_trigger + gamepad1.right_trigger));
-            } else {
-                lift.setPower(0);
-            }
+             */
+            liftTarget += Math.round(gamepad1.right_trigger-gamepad1.left_trigger) * 20;
         } else {
+            //auto
+            if (gamepad1.a) {
+                liftTarget = 0;
+            } else if (gamepad1.b) {
+                liftTarget = 1000;
+            } else if (gamepad1.x) {
+                liftTarget = 2000;
+            } else if (gamepad1.y) {
+                liftTarget = 3640;
+            }
+        }
+
+        //limits
+        if (liftTarget > 4000) {
+            liftTarget = 4000;
+        } else if (liftTarget < 0) {
+            liftTarget = 0;
+        }
+
+        //power setting
+        lift.setTargetPosition(liftTarget);
+
+        if (liftTarget > liftPos) {
+            lift.setPower(1);
+        } else if (liftPos > liftTarget) {
+            lift.setPower(-.6);
+        } else if (liftPos == liftTarget) {
             lift.setPower(0);
         }
+
 
         //intake
         if ((gamepad1.left_bumper) && intakePressed == 0) {
@@ -124,7 +145,7 @@ public class SoloDriverTeleOpAtl extends OpMode {
         if ((!gamepad1.left_bumper) && intakePressed > 0) {
             intakePressed = 0;
         }
-
+        //slow mode
         if ((gamepad1.right_bumper) && slowModePressed == 0) {
             slowMode = !slowMode;
             slowModePressed++;
@@ -132,6 +153,8 @@ public class SoloDriverTeleOpAtl extends OpMode {
         if ((!gamepad1.right_bumper) && slowModePressed > 0) {
             slowModePressed = 0;
         }
+
+
         telemetry.addData("slow Mode", slowMode);
         //telemetry
         telemetry.addData("lift position: ", liftPos);
