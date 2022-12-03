ffc74a8449c1b9cc893e86c8846eab3322940e3c
jihoo <0919imjihoon@gmail.com>
2022-12-02 21:56:39 -0600

    Auton Time Based

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/MoveRight.java
@@ -0,0 +1,72 @@
+package org.firstinspires.ftc.teamcode.Auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "MoveRight")
+
+public class MoveRight extends LinearOpMode {
+    //wheels
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public DcMotor leftBack = null;
+    public DcMotor rightBack = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        //go to corner
+        leftFront.setPower(.6);
+        leftBack.setPower(-.6);
+        rightFront.setPower(-.6);
+        rightBack.setPower(.6);
+
+        sleep(1000);
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        //goback
+        leftFront.setPower(-.6);
+        leftBack.setPower(.6);
+        rightFront.setPower(.6);
+        rightBack.setPower(-.6);
+
+        sleep(1000);
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+        //goForward
+        leftFront.setPower(0.3);
+        leftBack.setPower(0.3);
+        rightFront.setPower(0.3);
+        rightBack.setPower(0.3);
+
+        sleep(1500);
+
+        leftFront.setPower(0);
+        leftBack.setPower(0);
+        rightFront.setPower(0);
+        rightBack.setPower(0);
+
+
+    }
+}
