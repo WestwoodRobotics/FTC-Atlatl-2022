a3ef1ad81f359e708260df9027df75ff9e239233
jihoo <0919imjihoon@gmail.com>
2022-12-06 22:30:29 -0600

    rename opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/DoubleTeleDoubleMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/DoubleTeleDoubleMotor.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "DoubleAtl")
+@TeleOp(name = "DoubleMotorAtl")
 
 public class DoubleTeleDoubleMotor extends OpMode {
     //wheels
