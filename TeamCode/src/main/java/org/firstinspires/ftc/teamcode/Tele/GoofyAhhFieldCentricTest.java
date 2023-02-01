e835f7a96bdff6cb1a141e4011f18061719434f3
jihoo <0919imjihoon@gmail.com>
2023-02-01 00:25:17 -0600

    Field CleanUp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/GoofyAhhFieldCentricTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/GoofyAhhFieldCentricTest.java
@@ -32,15 +32,10 @@ package org.firstinspires.ftc.teamcode.Tele;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
